27863e50988ccdf9cdeff5d5b1e29e01d08ecb87
Chuyu Liu <chuyu@dropbox.com>
2020-05-14 19:03:07 -0400

    ios support share multiple files

--- ios/ReactNativeShareExtension.m
+++ ios/ReactNativeShareExtension.m
@@ -2,9 +2,10 @@
 #import "React/RCTRootView.h"
 #import <MobileCoreServices/MobileCoreServices.h>
 
-#define URL_IDENTIFIER @"public.url"
-#define IMAGE_IDENTIFIER @"public.image"
-#define TEXT_IDENTIFIER (NSString *)kUTTypePlainText
+static NSString *const FIELD_URI = @"value"; // uri
+static NSString *const FIELD_NAME = @"name";
+static NSString *const FIELD_TYPE = @"type";
+static NSString *const FIELD_SIZE = @"size";
 
 NSExtensionContext* extensionContext;
 
@@ -42,7 +43,7 @@ RCT_EXPORT_METHOD(close) {
 }
 
 
-
+// openURL is not used
 RCT_EXPORT_METHOD(openURL:(NSString *)url) {
   UIApplication *application = [UIApplication sharedApplication];
   NSURL *urlToOpen = [NSURL URLWithString:[url stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
@@ -55,74 +56,126 @@ RCT_REMAP_METHOD(data,
                  resolver:(RCTPromiseResolveBlock)resolve
                  rejecter:(RCTPromiseRejectBlock)reject)
 {
-    [self extractDataFromContext: extensionContext withCallback:^(NSString* val, NSString* contentType, NSException* err) {
+    [self extractDataFromContext: extensionContext withCallback:^(NSMutableArray* metadataArray, NSException* err) {
         if(err) {
             reject(@"error", err.description, nil);
         } else {
-            resolve(@{
-                      @"type": contentType,
-                      @"value": val
-                      });
+            resolve(metadataArray);
         }
     }];
 }
 
-- (void)extractDataFromContext:(NSExtensionContext *)context withCallback:(void(^)(NSString *value, NSString* contentType, NSException *exception))callback {
+- (void)extractDataFromContext:(NSExtensionContext *)context withCallback:(void(^)(NSMutableArray* metadataArray, NSException *exception))callback {
     @try {
         NSExtensionItem *item = [context.inputItems firstObject];
         NSArray *attachments = item.attachments;
 
-        __block NSItemProvider *urlProvider = nil;
-        __block NSItemProvider *imageProvider = nil;
-        __block NSItemProvider *textProvider = nil;
-
+        NSMutableArray *results = [NSMutableArray array];
+        NSUInteger totalItems = [attachments count];
+        
         [attachments enumerateObjectsUsingBlock:^(NSItemProvider *provider, NSUInteger idx, BOOL *stop) {
-            if([provider hasItemConformingToTypeIdentifier:URL_IDENTIFIER]) {
-                urlProvider = provider;
-                *stop = YES;
-            } else if ([provider hasItemConformingToTypeIdentifier:TEXT_IDENTIFIER]){
-                textProvider = provider;
-                *stop = YES;
-            } else if ([provider hasItemConformingToTypeIdentifier:IMAGE_IDENTIFIER]){
-                imageProvider = provider;
-                *stop = YES;
+            BOOL isLast = idx == totalItems - 1;
+            
+            // TODO add more file types
+            if ([provider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeImage]) {
+                 [provider loadItemForTypeIdentifier:(NSString *)kUTTypeImage options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                     [self processItem:item error:error results:results shouldInvokeCallback:isLast withCallback:callback];
+                 }];
+            } else if ([provider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeMovie]) {
+                [provider loadItemForTypeIdentifier:(NSString *)kUTTypeMovie options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    [self processItem:item error:error results:results shouldInvokeCallback:isLast withCallback:callback];
+                }];
+            } else if ([provider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeVideo]) {
+                [provider loadItemForTypeIdentifier:(NSString *)kUTTypeVideo options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    [self processItem:item error:error results:results shouldInvokeCallback:isLast withCallback:callback];
+                }];
+            } else if ([provider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeAudio]) {
+                [provider loadItemForTypeIdentifier:(NSString *)kUTTypeAudio options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    [self processItem:item error:error results:results shouldInvokeCallback:isLast withCallback:callback];
+                }];
+            } else if ([provider hasItemConformingToTypeIdentifier:(NSString *)kUTTypeText]) {
+                [provider loadItemForTypeIdentifier:(NSString *)kUTTypeText options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    [self processItem:item error:error results:results shouldInvokeCallback:isLast withCallback:callback];
+                }];
+            } else if ([provider hasItemConformingToTypeIdentifier:(NSString *)kUTTypePDF]) {
+                [provider loadItemForTypeIdentifier:(NSString *)kUTTypePDF options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
+                    [self processItem:item error:error results:results shouldInvokeCallback:isLast withCallback:callback];
+                }];
+            } else {
+                callback(nil, [NSException exceptionWithName:@"Error" reason:@"Couldn't find provider" userInfo:nil]);
             }
-        }];
-
-        if(urlProvider) {
-            [urlProvider loadItemForTypeIdentifier:URL_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
-                NSURL *url = (NSURL *)item;
-
-                if(callback) {
-                    callback([url absoluteString], @"text/plain", nil);
-                }
-            }];
-        } else if (imageProvider) {
-            [imageProvider loadItemForTypeIdentifier:IMAGE_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
-                NSURL *url = (NSURL *)item;
-
-                if(callback) {
-                    callback([url absoluteString], [[[url absoluteString] pathExtension] lowercaseString], nil);
-                }
-            }];
-        } else if (textProvider) {
-            [textProvider loadItemForTypeIdentifier:TEXT_IDENTIFIER options:nil completionHandler:^(id<NSSecureCoding> item, NSError *error) {
-                NSString *text = (NSString *)item;
-
-                if(callback) {
-                    callback(text, @"text/plain", nil);
-                }
-            }];
-        } else {
-            if(callback) {
-                callback(nil, nil, [NSException exceptionWithName:@"Error" reason:@"couldn't find provider" userInfo:nil]);
-            }
-        }
+         }];
+        
     }
     @catch (NSException *exception) {
-        if(callback) {
-            callback(nil, nil, exception);
+        callback(nil, exception);
+    }
+}
+
+
+- (void)processItem:(id<NSSecureCoding>) item
+                error:(NSError *)error
+                results:(NSMutableArray *)results
+                shouldInvokeCallback:(BOOL) shouldInvokeCallback
+                withCallback:(void(^)(NSMutableArray* metadataArray, NSException *exception))callback  {
+    if (error) {
+       callback(nil, [NSException exceptionWithName:@"loadItemForTypeIdentifier Error" reason:error.description userInfo:nil]);
+    }
+    
+    NSURL *url = (NSURL *)item;
+    NSMutableDictionary* result = [self getMetadataForUrl:url error:&error];
+    if (result) {
+        [results addObject:result];
+        if (shouldInvokeCallback) {
+            callback(results, nil);
         }
+    } else {
+        callback(nil, [NSException exceptionWithName:@"getMetadata Error" reason:error.description userInfo:nil]);
+    }
+}
+
+- (NSMutableDictionary *)getMetadataForUrl:(NSURL *)url error:(NSError **)error
+{
+    __block NSMutableDictionary* result = [NSMutableDictionary dictionary];
+    
+    [url startAccessingSecurityScopedResource];
+    
+    NSFileCoordinator *coordinator = [[NSFileCoordinator alloc] init];
+    __block NSError *fileError;
+    
+    [coordinator coordinateReadingItemAtURL:url options:NSFileCoordinatorReadingResolvesSymbolicLink error:&fileError byAccessor:^(NSURL *newURL) {
+        
+        if (!fileError) {
+            [result setValue:newURL.absoluteString forKey:FIELD_URI];
+            [result setValue:[newURL lastPathComponent] forKey:FIELD_NAME];
+            
+            NSError *attributesError = nil;
+            NSDictionary *fileAttributes = [[NSFileManager defaultManager] attributesOfItemAtPath:newURL.path error:&attributesError];
+            if(!attributesError) {
+                [result setValue:[fileAttributes objectForKey:NSFileSize] forKey:FIELD_SIZE];
+            } else {
+                NSLog(@"%@", attributesError);
+            }
+            
+            if ( newURL.pathExtension != nil ) {
+                CFStringRef extension = (__bridge CFStringRef)[newURL pathExtension];
+                CFStringRef uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, extension, NULL);
+                CFStringRef mimeType = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType);
+                CFRelease(uti);
+                
+                NSString *mimeTypeString = (__bridge_transfer NSString *)mimeType;
+                [result setValue:mimeTypeString forKey:FIELD_TYPE];
+            }
+        }
+    }];
+    
+    [url stopAccessingSecurityScopedResource];
+    
+    if (fileError) {
+        *error = fileError;
+        return nil;
+    } else {
+        return result;
     }
 }
 
